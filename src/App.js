0b7a2ed2420cdf12b985b04f0b6dd0f2b4555037
Mohd Imran <mohd78696321@gmail.com>
2021-09-20 12:32:08 +0530

    update: app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+var emojiDictionary = {
+  "😀": "Grinning Face",
+  "😁": "Beaming Face with Smiling Eyes",
+  "😅": "Grinning Face with Sweat",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "😉": "Winking Face",
+  "😇": "Smiling Face with Halo",
+  "🤩": "Star-Struck",
+  "😛": "Face with Tongue",
+  "😪": "Sleepy Face",
+  "😳": "Flushed Face",
+  "🥱": "Yawning Face",
+  "😫": "Tired Face",
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
 const App = () => {
+  const [meaning, setMeaning] = useState("");
+
+  function inputClickHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      return "We don't have this emoji in our database.";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+
+    setMeaning(meaning);
+  }
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <h1>Emoji Interpreter</h1>
+      <input onChange={inputClickHandler} placeholder="Enter emoji" />
+      <h2>Meaning: {meaning}</h2>
+      <h3>Emojis We Know</h3>
+
+      {emojisWeKnow.map((emoji) => {
+        return (
+          <span onClick={() => emojiClickHandler(emoji)} key={emoji}>
+            {emoji}
+          </span>
+        );
+      })}
+    </>
   );
 };
 
